9f588080e89817e44d2a2e72fee57429f0362be3
Fadie Hannona <fadiehannona@gmail.com>
2015-04-21 13:50:01 +0100

    it creates the newsletters model data

--- test/unit/newsletterSpec.js
+++ test/unit/newsletterSpec.js
@@ -2,6 +2,29 @@
 
 describe('NewsletterCtrl', function() {
 
-  beforeEach(module('newsletter'));
+  beforeEach(module('newsletters'));
 
-})
+  describe('NewsletterCtrl', function() {
+
+    var scope, ctrl, $httpBackend;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('newsletters/newsletters.json').respond([{
+        name: 'React.JS London', id: '0', tag1: 'Javasccript', tag2: 'Front-End', tag3: 'London'
+      }]);
+
+      scope = $rootScope.$new()
+      ctrl = $controller('NewsletterCtrl', {$scope: scope});
+
+    }));
+
+    it('creates the newsletters model data', function() {
+      expect(scope.newsletters).toBeUndefined();
+      $httpBackend.flush();
+      expect(scope.newsletters).toEqual([{ name: 'React.JS London', id: '0', tag1: 'Javasccript', tag2: 'Front-End', tag3: 'London' }])
+    });
+
+  })
+
+});
