4b27aec6b5ce0075b795a1e0b40d070f5f47e423
Fadie Hannona <fadiehannona@gmail.com>
2015-04-21 13:07:01 +0100

    has names and tags for newsletters

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -5,8 +5,24 @@ describe('newsletter app', function() {
     newsletter = element.all(by.repeater('newsletter in newsletters'));
   });
 
-  it('has a list of newsletters', function() {
+  it('has a list of 6 newsletters', function() {
     expect(newsletter.get(0).isPresent()).toBe(true);
   });
+
+  it('has a name for newsletters', function() {
+    expect(newsletter.get(0).getText()).toContain('React.JS London')
+  })
+
+  it('has tag 1 for newsletters', function() {
+    expect(newsletter.get(0).getText()).toContain('Javascript')
+  })
+
+  it('has tag 2 for newsletters', function() {
+    expect(newsletter.get(0).getText()).toContain('Front-End')
+  })
+
+  it('has tag 3 for newsletters', function() {
+    expect(newsletter.get(0).getText()).toContain('London')
+  })
 
 });
